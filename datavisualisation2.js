865026e7dc0668f04ae0cf40d8b8e52bea821c6a
shew0015 <111554720+shew0015@users.noreply.github.com>
2022-10-13 10:20:14 +1100

    Add files via upload

---
+++ datavisualisation2.js
@@ -0,0 +1,34 @@
+var vg_1 = "windspeedyear.json";
+vegaEmbed("#vis1", vg_1, {"actions": false}).then(function (result) {
+}
+).catch(console.error);
+
+var vg_2 = "melbournewindspeedhumidity.json";
+vegaEmbed("#vis2", vg_2, {"actions": false}).then(function (result) {
+}
+).catch(console.error);
+
+var vg_3 = "melbournewindspeedpressure.json";
+vegaEmbed("#vis3", vg_3, {"actions": false}).then(function (result) {
+}
+).catch(console.error);
+
+var vg_4 = "melbournerainfalldate.json";
+vegaEmbed("#vis4", vg_4, {"actions": false}).then(function (result) {
+}
+).catch(console.error);
+
+var vg_5 = "melbournerainfallyear.json";
+vegaEmbed("#vis5", vg_5, {"actions": false}).then(function (result) {
+}
+).catch(console.error);
+
+var vg_6 = "melbournemaxtempdate.json";
+vegaEmbed("#vis6", vg_6, {"actions": false}).then(function (result) {
+}
+).catch(console.error);
+
+var vg_7 = "melbournemaxtempyear.json";
+vegaEmbed("#vis7", vg_7, {"actions": false}).then(function (result) {
+}
+).catch(console.error);
